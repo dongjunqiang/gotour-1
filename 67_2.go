965f5b13764d418b123cc607232cd453652ac1fd
Junlong Xie <decimalbell@gmail.com>
2014-10-18 17:41:31 +0800

    67_2

---
+++ 67_2.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+    c := make(chan int)
+    fmt.Println("cap", cap(c), "len", len(c))
+    ch := make(chan int, 64)
+    fmt.Println("cap", cap(ch), "len", len(ch))
+    for i := 0; i < 42; i++ {
+        ch <- i
+    }
+    fmt.Println("cap", cap(ch), "len", len(ch))
+}
