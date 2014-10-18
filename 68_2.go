22a23fe23d7376aab4f5c87f5321e7e0a2394dbb
Junlong Xie <decimalbell@gmail.com>
2014-10-18 18:18:37 +0800

    68_2

---
+++ 68_2.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func f(i int) {
+    for {
+        fmt.Println("goroutine", i)
+    }
+}
+
+func main() {
+    for i := 0; i < 23; i++ {
+        go f(i)
+    }
+
+    for {
+        fmt.Println("main")
+    }
+}
