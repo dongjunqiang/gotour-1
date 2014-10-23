60c64148485511189789017ef4e8574f6322f5b4
Junlong Xie <decimalbell@gmail.com>
2014-10-23 23:59:02 +0800

    52

---
+++ 52.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+	v := &Vertex{3, 4}
+	vv := Vertex{6, 8}
+	fmt.Println(v.Abs())
+	fmt.Println(vv.Abs())
+}
